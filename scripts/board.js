ea3fb5f14282002aaeb985506615a8402a8af464
keramsky <keramsky@yahoo.com>
2024-04-25 20:13:42 +0200

    add player jump

--- scripts/board.js
+++ scripts/board.js
@@ -2,8 +2,11 @@ const canvas = document.querySelector(".board");
 const ctx = canvas.getContext("2d");
 
 let x = 475;
-let y = 450;
+let y = canvas.height - 50;
 let vx = 0;
+let vy = 0;
+
+let grounded = true;
 
 function update(){
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
@@ -14,7 +17,25 @@ function update(){
     } else {
         x += vx;
     }
+    y += vy;
+
+    collision();
     ctx.fillRect(x, y, 50, 50);
     requestAnimationFrame(update);
 }
+
+function collision(){
+    if(y > canvas.height - 50){
+        y = canvas.height - 50;
+        grounded = true;
+        vy = 0;
+    }
+}
+
 update();
+
+setInterval(function Gravity(){
+    if(!grounded){
+        vy += 0.05;
+    }
+});
--- scripts/movement.js
+++ scripts/movement.js
@@ -1,6 +1,8 @@
 addEventListener("keydown", function(e){
     if(e.code === "KeyD" || e.code === "ArrowRight") vx = 5;
     if(e.code === "KeyA" || e.code === "ArrowLeft") vx = -5;
+    if(e.code === "Space" || e.code === "KeyW" || e.code === "ArrowUp")
+        if(grounded === true) vy = -5, grounded = false;
 });
 
 addEventListener("keyup", function(e){
