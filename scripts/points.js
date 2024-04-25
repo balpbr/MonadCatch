aa2d31fc8e62e9e4aa3adf31a1314e14c18ac8a3
keramsky <keramsky@yahoo.com>
2024-04-25 22:17:42 +0200

    add infinite blocks falling

--- scripts/points.js
+++ scripts/points.js
@@ -1,41 +1,52 @@
-const block = document.createElement("div");
-block.classList.add("block");
-const blockParent = document.querySelector(".board");
+setInterval(spawnBlock, 1000);
 
-blockParent.appendChild(block);
+function spawnBlock(){
 
-block.style.backgroundColor = "white";
+    const block = document.createElement("div");
+    block.classList.add("block");
+    const blockParent = document.querySelector(".board");
+    
+    blockParent.appendChild(block);
+    
+    block.style.backgroundColor = "white";
+    
+    let x2 = Math.floor(Math.random() * (canvas.width - 50));
+    block.style.left = `${x2}px`;
+    
+    let vy2 = 0;
+    let y2 = 0;
+    let grounded2 = false;
+    
+    setInterval(function Gravity(){
+        blockCollision(y2, vy2);
+        if(blockCollision(y2, vy2) === true) {
+            grounded2 = true;
+        }
+        checkIfPlayerTouch(x2, y2, block);
+    
+        if(!grounded2){
+            vy2 += 0.005;
+            y2 += vy2;
+            block.style.top = `${y2}px`;
+        }
+    });
+}
+spawnBlock();
 
-let x2 = Math.floor(Math.random() * (canvas.width - 50));
-block.style.left = `${x2}px`;
-
-let vy2 = 0;
-let y2 = 0;
-let grounded2 = false;
-
-setInterval(function Gravity(){
-    blockCollision();
-    checkIfPlayerTouch();
-
-    if(!grounded2){
-        vy2 += 0.005;
-        y2 += vy2;
-        block.style.top = `${y2}px`;
-    }
-});
-
-function checkIfPlayerTouch(){
-    if(y2 + 25 >= y){
-        if(x2 + 25 >= x && x2 <= x + 50){
-            block.remove();
+function checkIfPlayerTouch(xBlock, yBlock, blockElement){
+    if(yBlock + 25 >= y){
+        if(xBlock + 25 >= x && xBlock <= x + 50){
+            blockElement.remove();
         }
     }
 }
 
-function blockCollision(){
-    if(y2 >= canvas.height - 45){
-        y2 = canvas.height - 45;
-        grounded2 = true;
-        vy2 = 0;
+function blockCollision(yBlock, vyBlock){
+    if(yBlock >= canvas.height - 45){
+        yBlock = canvas.height - 45;
+        vyBlock = 0;
+        return true;
+    } else {
+        return false;
     }
 }
