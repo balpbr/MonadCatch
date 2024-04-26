325505fc477e10c1d6f54f95b9df3c1f5927ba73
keramsky <keramsky@yahoo.com>
2024-04-26 14:29:06 +0200

    add score

--- scripts/points.js
+++ scripts/points.js
@@ -1,5 +1,7 @@
 setInterval(spawnBlock, 1000);
 
+let score = 0;
+
 function spawnBlock(){
 
     const block = document.createElement("div");
@@ -17,12 +19,16 @@ function spawnBlock(){
     let y2 = 0;
     let grounded2 = false;
     
-    setInterval(function Gravity(){
+    let blockInterval = setInterval(function Gravity(){
         blockCollision(y2, vy2);
         if(blockCollision(y2, vy2) === true) {
             grounded2 = true;
+            setTimeout(() => {
+                block.remove();
+                clearInterval(blockInterval);
+            }, 1000)
         }
-        checkIfPlayerTouch(x2, y2, block);
+        checkIfPlayerTouch(x2, y2, block, blockInterval);
     
         if(!grounded2){
             vy2 += 0.005;
@@ -33,10 +39,14 @@ function spawnBlock(){
 }
 spawnBlock();
 
-function checkIfPlayerTouch(xBlock, yBlock, blockElement){
-    if(yBlock + 25 >= y){
+function checkIfPlayerTouch(xBlock, yBlock, blockElement, blockInterval){
+    if(yBlock + 25 >= y && yBlock <= y + 50){
         if(xBlock + 25 >= x && xBlock <= x + 50){
             blockElement.remove();
+            clearInterval(blockInterval);
+            score++;
+            const scoreText = document.querySelector(".score");
+            scoreText.innerHTML = `${score}`;
         }
     }
 }
